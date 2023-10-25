56ed28e5d458e3c82077c17d2f18ddad6015319c
huang <401756536@qq.com>
2023-10-25 10:44:46 +0800

    second commit

--- src/main/java/com/ruanjian/git/git.java
+++ src/main/java/com/ruanjian/git/git.java
@@ -3,5 +3,6 @@ package com.ruanjian.git;
 public class git {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git1!");
     }
 }
