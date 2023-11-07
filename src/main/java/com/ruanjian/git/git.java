31cab11a194f4d3a0d9b21a1c0f0813b79defd1b
yuebuqun1234321 <145423570+yuebuqun1234321@users.noreply.github.com>
2023-11-07 14:15:28 +0800

    Update git.java

--- src/main/java/com/ruanjian/git/git.java
+++ src/main/java/com/ruanjian/git/git.java
@@ -8,5 +8,6 @@ public class git {
         System.out.println("hello git3!");
         System.out.println("master test");
         System.out.println("hot fix test");
+        System.out.println("pull test");
     }
 }
